49bd5739a489fc6736596fa91f67f672bdc18c95
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-07-01 15:02:25 -0400

    completed half of the task

--- server/app.py
+++ server/app.py
@@ -25,5 +25,35 @@ db.init_app(app)
 def home():
     return ''
 
+@app.route('/campers', methods=['GET', 'POST'])
+def all_campers():
+    campers = Camper.query.all()
+    
+    return [c.to_dict(rules=['-signups']) for c in campers], 200
+
+@app.route('/campers/<int:id>', methods=['GET', 'PATCH'])
+def campers_by_id(id):
+    camper = Camper.query.filter(Camper.id == id).first()
+    if not camper:
+            return {'error':'Camper not found'}, 404
+        
+    if request.method == 'GET':
+        return camper.to_dict(), 200
+    elif request.method == 'PATCH':
+        data = request.get_json()
+        
+        try:
+            if 'name' in data:
+                camper.name = data['name']
+            if 'age' in data:
+                camper.age = data['age']
+        except ValueError:
+            return {'error':["validation error"]}, 400
+            
+        db.session.add(camper)
+        db.session.commit()
+        
+        return camper.to_dict(rules=['-signups']), 202
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
